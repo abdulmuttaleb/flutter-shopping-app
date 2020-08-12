2a9bc5b3f39f9b12fa8e914c3211602a6e8e657b
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-08-12 11:44:33 +0200

    - showing favorite products and all products functionality

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -3,19 +3,23 @@ import 'package:provider/provider.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/product_item.dart';
 class ProductsGrid extends StatelessWidget {
+  final bool _showFavorites;
+
+  ProductsGrid(this._showFavorites);
+
   @override
   Widget build(BuildContext context) {
 
     final productsData = Provider.of<Products>(context);
-    final loadedProducts = productsData.items;
+    final loadedProducts = _showFavorites? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
           mainAxisSpacing: 10),
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (ctx) => loadedProducts[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        value: loadedProducts[i],
         child: ProductItem(),
       ),
       padding: const EdgeInsets.all(10.0),
