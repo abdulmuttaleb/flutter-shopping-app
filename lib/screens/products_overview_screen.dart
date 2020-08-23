efc1d0baea3c97ebe3038fbd380da3d013356d14
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-08-23 20:27:45 +0200

    - cart screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/products.dart';
+import 'package:shop_app/screens/cart_screen.dart';
 import 'package:shop_app/widgets/badge.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
@@ -48,7 +49,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                     child: ch,
                     value: cartData.itemCount.toString(),
                   ),
-              child: IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){},),
+              child: IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },),
             )
           ],
         ),
